3293cb9b21dbe1957c2ea5fcffe2b2b4f31bec5d
mazhar rayhan <mazharrayhan3737@gmail.com>
2025-09-04 17:49:52 +0600

    db fixed

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 # DATABASE_URL = "sqlite:///./estimation.db"
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./estimation.db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://rhd_estimation_db_user:REInwLwpx6bOuOabJvbnesC04y74jgC5@dpg-d2smeuf5r7bs73akn6g0-a.singapore-postgres.render.com/rhd_estimation_db")
 
 class Base(DeclarativeBase):
     pass
