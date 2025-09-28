cbaf3b8e9be3a6b450ebb2d8af6f34655a9a79fe
mazhar rayhan <mazharrayhan3737@gmail.com>
2025-09-28 17:34:44 +0600

    database issue hopefully fixed

--- app/database.py
+++ app/database.py
@@ -2,23 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
-
-if ENVIRONMENT == "production":
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-else:
-    DATABASE_URL = "sqlite:///./estimation.db"
-
-class Base(DeclarativeBase):
-    pass
-
-# Add this block to handle SSL for production
-if DATABASE_URL.startswith("postgresql://"):
-    engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
-else:
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://rhd_estimation_db_user:REInwLwpx6bOuOabJvbnesC04y74jgC5@dpg-d2smeuf5r7bs73akn6g0-a.singapore-postgres.render.com/rhd_estimation_db")
 
 print(f"Using database at {DATABASE_URL}")
